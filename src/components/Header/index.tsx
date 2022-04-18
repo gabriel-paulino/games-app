f35e220465c18d1bdbedac14c8a65f3ac73f528c
gabriel-paulino <gabriel.paulino104@gmail.com>
2022-04-18 19:47:42 -0300

    Configurações gerais

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Title } from './styles';
+
+function Header() {
+  return (
+    <Container>
+      <Title>Crud</Title>
+    </Container>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 60px;
+  background: #222;
+  align-items: center;
+`;
+export const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #fff;
+`;
